109ff6184e65222a914d14130fcb9b07c35c99ba
himhim1113 <beyondlilith@gmail.com>
2020-04-14 22:18:03 +0900

    yattaze

--- deepbot.py
+++ deepbot.py
@@ -1,35 +1,36 @@
 # インストールした discord.py を読み込む
-import discord
-#import requests
+from discord.ext import commands
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = 'Njk5NTg1NjcyNTc4NTk2OTc0.XpWl6Q.QstVlSWVekdLUhg_fieZGuG_5Mg'
+prefix='/'
 
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
+class DeepBot(commands.Cog):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+	
+    # 起動時に動作する処理
+    #@client.event
+    #async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+        #print('ログインしました')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-    '''
-    if message.content == '/pic':
-        r = requests.get(url, stream=True)
-        if r.status_code == 200:
-            with open(filename, 'wb') as f:
-                f.write(r.content)
-    '''
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+    @commands.command()
+    async def neko(self, ctx):
+	'''にゃーん'''
+	await ctx.send(f'にゃーん')
+
+    @commands.command()
+    async def hello(self, ctx):
+        '''出会いのあいさつをする'''
+        await ctx.send(f"どうも、{ctx.author.name}さん!")
+
+    @commands.command()
+    async def goodbye(self, ctx):
+        '''別れの挨拶をする'''
+        await ctx.send(f"じゃあね、{ctx.author.name}さん!")
+
+bot = commands.Bot(command_prefix=prefix)
+bot.add_cog(DeepBot(bot=bot))
+bot.run(TOKEN)
